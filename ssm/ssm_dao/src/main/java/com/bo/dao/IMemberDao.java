ef63723ef13f9d1c2dd0d6f3bbcc40df59afaaf2
lvbowen <1440089582@qq.com>
2019-06-23 19:42:03 +0800

    第er次

---
+++ ssm/ssm_dao/src/main/java/com/bo/dao/IMemberDao.java
@@ -0,0 +1,12 @@
+package com.bo.dao;
+
+        import com.bo.domain.Member;
+        import org.apache.ibatis.annotations.Select;
+
+public interface IMemberDao {
+    @Select("select * from member where id=#{id}")
+    public Member findById( String id) throws Exception;
+
+}
+
+
